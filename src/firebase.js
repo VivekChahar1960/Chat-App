36ef6abae1a307c702819bfb885b67367a0709f7
Vivek Chahar <78673919+VivekChahar1960@users.noreply.github.com>
2025-05-06 17:07:15 +0530

    changes firebaseconfig

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,6 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getDatabase, ref, onValue, push } from 'firebase/database';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from 'firebase/app';
+import { getDatabase, ref, onValue, push, set } from 'firebase/database';
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyApn-U8Sy9XWSUk2hYVrfCJKNWas2msDoY",
   authDomain: "superchat-76ef9.firebaseapp.com",
@@ -17,8 +12,7 @@ const firebaseConfig = {
   measurementId: "G-99KPVPSFSB"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 
-export { db, ref, onValue, push };
+export { db, ref, onValue, push, set };
